135258795a11741808ed6fbd0fa9386358cc791b
totallyrat <119330461+totallyrat@users.noreply.github.com>
2026-07-28 01:10:54 +0200

    Bump PUMPE release to v5.3.0

--- config.lua
+++ config.lua
@@ -2,7 +2,7 @@
 -- Copy this file with the rest of the project to each ComputerCraft computer.
 
 return {
-    version = "5.2.1",
+    version = "5.3.0",
     protocol = "PUMPE_BANK_V5",
     hostname = "BANK_SERVER",
     data_file = "bank_data_v5.dat",
@@ -14,6 +14,9 @@ return {
     gps_ttl_ms = 90 * 1000,
     proximity_update_seconds = 2,
     max_ticket_quantity = 5,
+    max_territories_per_account = 3,
+    visa_min_days = 1,
+    visa_max_days = 30,
 
     pin_free_limit = 50,
     daily_spend_limit = 5000,
